41f668cdb26d5d94a4f98d23dd3728b81c697a85
Micah Riggan <mriggan@marqeta.com>
2021-03-17 11:11:38 -0400

    set user nonce, and determine capital requirement from PUT/CALL

--- contracts/ShrubExchange.sol
+++ contracts/ShrubExchange.sol
@@ -9,6 +9,30 @@ contract OptionsExchange {
     CALL
   }
 
+  // Data that is common between a buy and sell
+  struct OrderCommon {
+    address baseAsset;      // ETH-USD, USD is the base
+    address quoteAsset;     // ETH-USD ETH is the quote
+    uint expiry;            // timestamp expires
+    uint strike;            // The price of the pair
+    OptionType optionType;
+  }
+
+
+  // Meant to be hashed with OrderCommon
+  struct SmallOrder {
+    uint size;
+    bool isBuy;
+    uint nonce;             // unique id of order
+    uint price;
+    uint offerExpire;       // time this order expires
+    uint fee;               // matcherFee
+
+    uint8 v;
+    bytes32 r;
+    bytes32 s;
+  }
+
   struct Order {
     uint size;
     bool isBuy;
@@ -28,9 +52,10 @@ contract OptionsExchange {
     bytes32 s;
   }
 
-  mapping(address => mapping(address => mapping(address => uint))) userPairNonce;
-  mapping(address => mapping(address => uint)) userTokenBalances;
-  mapping(address => mapping(address => uint)) userTokenLockedBalance;
+  event OrderMatched(address seller, address buyer, SmallOrder sellOrder, SmallOrder buyOrder, OrderCommon common);
+  mapping(address => mapping(address => mapping(address => uint))) public userPairNonce;
+  mapping(address => mapping(address => uint)) public userTokenBalances;
+  mapping(address => mapping(address => uint)) public userTokenLockedBalance;
 
   bytes32 private constant SALT = keccak256("0x43efba454ccb1b6fff2625fe562bdd9a23260359");
   bytes private constant EIP712_DOMAIN = "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract,bytes32 salt)";
@@ -44,7 +69,7 @@ contract OptionsExchange {
     SALT
   ));
 
-  bytes32 private constant ORDER_TYPEHASH = keccak256("Order(uint size, address signer, bool isBuy, uint nonce, uint price, uint offerExpire, uint fee,              address baseAsset, address quoteAsset, uint expiry, uint strike, OptionType optionType, uint8 v, bytes32 r, bytes32 s)");
+  bytes32 private constant ORDER_TYPEHASH = keccak256("Order(uint size, address signer, bool isBuy, uint nonce, uint price, uint offerExpire, uint fee, address baseAsset, address quoteAsset, uint expiry, uint strike, OptionType optionType, uint8 v, bytes32 r, bytes32 s)");
 
   function hashOrder(Order memory order) public pure returns (bytes32) {
     return keccak256(abi.encode(
@@ -64,16 +89,38 @@ contract OptionsExchange {
     ));
   }
 
-  modifier orderMatches(Order memory sellOrder, Order memory buyOrder) {
+
+  function hashSmallOrder(SmallOrder memory order, OrderCommon memory common) public pure returns (bytes32) {
+    return keccak256(abi.encode(
+      ORDER_TYPEHASH,
+      order.size,
+      order.isBuy,
+      order.nonce,
+      order.price,
+      order.offerExpire,
+      order.fee,
+
+      common.baseAsset,
+      common.quoteAsset,
+      common.expiry,
+      common.strike,
+      common.optionType
+    ));
+  }
+
+  function getCurrentNonce(address user, address quoteAsset, address baseAsset) public view returns(uint) {
+    return userPairNonce[user][quoteAsset][baseAsset];
+  }
+
+  function getAvailableBalance(address user, address asset) public view returns(uint) {
+    return userTokenBalances[user][asset] - userTokenLockedBalance[user][asset];
+  }
+
+  modifier orderMatches(SmallOrder memory sellOrder, SmallOrder memory buyOrder, OrderCommon memory common) {
     require(sellOrder.isBuy == false, "Sell order should not be buying");
     require(buyOrder.isBuy == true, "Buy order should be buying");
-    require(sellOrder.baseAsset == buyOrder.baseAsset, "Base Must Match");
-    require(sellOrder.quoteAsset == buyOrder.quoteAsset, "Quote Must Match");
-    require(sellOrder.expiry == buyOrder.expiry, "Expiration must match");
-    require(sellOrder.strike == buyOrder.strike, "Strike price must match");
-    require(sellOrder.price <= buyOrder.price, "Price must be sufficient for seller");
-    require(sellOrder.optionType == buyOrder.optionType, "Must be the same option type");
 
+    require(sellOrder.price <= buyOrder.price, "Price must be sufficient for seller");
     require(sellOrder.size >= buyOrder.size, "Cannot buy more than being sold");
     require(sellOrder.offerExpire <= block.timestamp, "Sell order has expired");
     require(buyOrder.offerExpire <= block.timestamp, "Buy order has expired");
@@ -81,9 +128,24 @@ contract OptionsExchange {
     _;
   }
 
-  function matchOrder(Order memory sellOrder, Order memory buyOrder) orderMatches(sellOrder, buyOrder) public {
-    address seller = ecrecover(hashOrder(sellOrder), sellOrder.v, sellOrder.r, sellOrder.s);
-    address buyer = ecrecover(hashOrder(buyOrder), buyOrder.v, buyOrder.r, buyOrder.s);
-    userTokenLockedBalance[seller][address(0x0)] += sellOrder.size;
+  function matchOrder(SmallOrder memory sellOrder, SmallOrder memory buyOrder, OrderCommon memory common) orderMatches(sellOrder, buyOrder, common) public {
+    address seller = ecrecover(hashSmallOrder(sellOrder, common), sellOrder.v, sellOrder.r, sellOrder.s);
+    address buyer = ecrecover(hashSmallOrder(buyOrder, common), buyOrder.v, buyOrder.r, buyOrder.s);
+    require(getCurrentNonce(seller, common.quoteAsset, common.baseAsset) == sellOrder.nonce - 1);
+    require(getCurrentNonce(buyer, common.quoteAsset, common.baseAsset) == buyOrder.nonce - 1);
+
+    // TODO: Lockup capital
+    if(common.optionType == OptionType.CALL) {
+      require(getAvailableBalance(seller, common.quoteAsset) > sellOrder.size, "Seller must have enough free collateral");
+      userTokenLockedBalance[seller][common.quoteAsset] += sellOrder.size;
+    }
+    if(common.optionType == OptionType.PUT) {
+      require(getAvailableBalance(seller, common.baseAsset) > sellOrder.size * common.strike, "Seller must have enough free collateral");
+      userTokenLockedBalance[seller][common.baseAsset] += sellOrder.size * common.strike;
+    }
+
+    emit OrderMatched(seller, buyer, sellOrder, buyOrder, common);
+    userPairNonce[buyer][common.quoteAsset][common.baseAsset] = buyOrder.nonce;
+    userPairNonce[seller][common.quoteAsset][common.baseAsset] = sellOrder.nonce;
   }
 }
