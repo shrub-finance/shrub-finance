e3234aabb2abc42430474b188622212b4d4ca24a
John Guthrie <jguthrie7@gmail.com>
2021-05-12 23:59:45 -0400

    Update fakeToken to have initial supply of 10000

--- contracts/FakeToken.sol
+++ contracts/FakeToken.sol
@@ -4,6 +4,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FakeToken is ERC20 {
   constructor() ERC20("FAKE", "FK") public {
-    _mint(msg.sender, 10000);
+    _mint(msg.sender, 10000e18);
   }
 }
