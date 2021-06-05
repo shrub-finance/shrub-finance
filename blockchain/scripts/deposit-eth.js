16a5dbe2c567a03cc548ee8686261d017cb03cdf
Micah Riggan <mriggan@marqeta.com>
2021-06-05 13:33:00 -0400

    Cleanup unused code

--- blockchain/scripts/deposit-eth.js
+++ blockchain/scripts/deposit-eth.js
@@ -1,8 +1,6 @@
-const { Shrub712 } = require('../utils/EIP712');
 const ExchangeJson = require('../build/contracts/ShrubExchange.json');
 const Web3 = require('web3');
 const util = require('util');
-const fetch = require('node-fetch');
 
 const wsUrl = "http://127.0.0.1:8545"
 const web3 = new Web3(new Web3.providers.HttpProvider(wsUrl));
@@ -16,35 +14,10 @@ const Assets = {
 
 const wait = util.promisify(setTimeout);
 
-async function generateRandomOrder(nonce) {
-  return {
-    nonce,
-    size: Math.random() * 5,
-    isBuy: Math.random() * 100 > 50,
-    price: Math.floor(Math.random() * 4000),
-    offerExpire: Math.floor((new Date().getTime() + 5 * 1000 * 60) / 1000),
-    fee: Math.floor(Math.random() * 100),
-    baseAsset: Assets.USDC,
-    quoteAsset: Assets.ETH,
-    expiry: Math.floor((new Date().getTime() + 30 * 1000 * 60) / 1000),
-    strike: Math.floor(Math.random() * 4000),
-    optionType: Math.floor(Math.random() * 2),
-  }
-}
-
-async function saveOrder(order) {
-  await fetch(`http://localhost:${apiPort}/orders`, {
-    method: 'post',
-    body:    JSON.stringify(order),
-    headers: { 'Content-Type': 'application/json' },
-  })
-}
-
 async function main() {
   const currentNetwork = await web3.eth.net.getId();
   const exchangeAddress = ExchangeJson.networks[currentNetwork.toString()].address;
   console.log("Using ShrubExchange:", {exchangeAddress});
-  const shrubInterface = new Shrub712(17, exchangeAddress);
   const exchange = new web3.eth.Contract(ExchangeJson.abi, exchangeAddress);
   const depositAmount = process.env.DEPOSIT_ETH || "1";
   const wei = web3.utils.toWei(depositAmount, "ether");
