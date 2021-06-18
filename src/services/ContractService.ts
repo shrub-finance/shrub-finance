a59551cacb2ee17942351fb065ba8b616945223d
Micah Riggan <mriggan@marqeta.com>
2021-06-17 20:32:38 -0400

    Fix compile issues

--- src/services/ContractService.ts
+++ src/services/ContractService.ts
@@ -1,46 +1,59 @@
 import { EventEmitter } from "events";
-import Web3 from 'web3';
+import Web3 from "web3";
 import { Config } from "../config";
 import { Orders } from "./OrderService";
 const web3Url = Config.web3WssUrl;
 const web3 = new Web3(new Web3.providers.WebsocketProvider(web3Url));
-import ExchangeJson from '../../artifacts/contracts/ShrubExchange.sol/ShrubExchange.json';
-const abi = ExchangeJson.abi;
+import ExchangeJson from "../../artifacts/contracts/ShrubExchange.sol/ShrubExchange.json";
+import { AbiItem } from "web3-utils/types";
+import { ShrubExchange } from "../types/ShrubExchange";
+const abi = ExchangeJson.abi as AbiItem[];
 
 export class ContractService {
   public match: EventEmitter;
   public exchange: any;
 
-
   async start() {
     const address = await this.getExchangeAddress();
-    this.exchange = new web3.eth.Contract(abi, address) as any as any;
+    this.exchange = new web3.eth.Contract(abi, address) as any as ShrubExchange;
     this.match = this.exchange.events.OrderMatched((err, order) => {
-      const {buyer, seller} = order.returnValues;
-      const buyNonce = order.returnValues.buyOrder["nonce"];
-      const sellNonce = order.returnValues.sellOrder["nonce"];
-      console.log("Order matched", "Buyer", order.returnValues.buyer, "Seller", order.returnValues.seller);
-      console.log("Buyer nonce", order.returnValues.buyOrder["nonce"]);
-      console.log("Seller nonce", order.returnValues.sellOrder["nonce"]);
+      const { buyer, seller } = order.returnValues;
+      const buyNonce = order.returnValues.buyOrder.nonce;
+      const sellNonce = order.returnValues.sellOrder.nonce;
+      console.log(
+        "Order matched",
+        "Buyer",
+        order.returnValues.buyer,
+        "Seller",
+        order.returnValues.seller
+      );
+      console.log("Buyer nonce", order.returnValues.buyOrder.nonce);
+      console.log("Seller nonce", order.returnValues.sellOrder.nonce);
       Orders.fufillOrder(order);
     });
   }
 
   async getExchangeAddress() {
-    if(Config.shrubExchangeAddress) {
+    if (Config.shrubExchangeAddress) {
       return Config.shrubExchangeAddress;
     }
     const currentNetwork = await web3.eth.net.getId();
-    const exchangeAddress = ExchangeJson.networks[currentNetwork.toString()].address;
+    const exchangeAddress = process.env.SHRUB_ADDRESS;
     return exchangeAddress;
   }
 
   async getFilledOrders(address: string) {
     const [sells, buys] = await Promise.all([
-      this.exchange.events.OrderMatched({fromBlock: 0, filter: {seller: address}}),
-      this.exchange.events.OrderMatched({fromBlock: 0, filter: {buyer: address}}),
+      this.exchange.events.OrderMatched({
+        fromBlock: 0,
+        filter: { seller: address },
+      }),
+      this.exchange.events.OrderMatched({
+        fromBlock: 0,
+        filter: { buyer: address },
+      }),
     ]);
-    return {sells, buys};
+    return { sells, buys };
   }
 
   stop() {
