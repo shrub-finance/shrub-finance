e3fb2cd5b2e4f57c6bf385ed196d3cac302c5936
way2rach <way2rachita@gmail.com>
2021-07-05 19:17:16 -0400

    remove unused components

--- packages/app/src/components/TopNav.tsx
+++ packages/app/src/components/TopNav.tsx
@@ -33,7 +33,7 @@ const NavLink = ({ children }: { children: ReactNode }) => (
       textDecoration: "none",
       bg: useColorModeValue("gray.200", "gray.700"),
     }}
-    href={"/"}
+    // href={"/"}
   >
     {children}
   </Link>
