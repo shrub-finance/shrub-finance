a6f7e7b5694859685a7f7599f5fd90944416ab3e
way2rach <way2rachita@gmail.com>
2021-08-20 10:28:22 -0400

    comment out fortmatic and portis for the time being

--- packages/app/src/components/ConnectWallet.tsx
+++ packages/app/src/components/ConnectWallet.tsx
@@ -48,8 +48,8 @@ enum ConnectorNames {
     WalletConnect = "Wallet Connect",
     CoinbaseWallet = "Coinbase Wallet",
     Ledger = "Ledger",
-    Portis= "Portis",
-    Fortmatic= "Fortmatic"
+    // Portis= "Portis",
+    // Fortmatic= "Fortmatic"
 
 }
 const connectorsByName: { [connectorName in ConnectorNames]: any } = {
@@ -57,8 +57,8 @@ const connectorsByName: { [connectorName in ConnectorNames]: any } = {
     [ConnectorNames.WalletConnect]: walletconnect,
     [ConnectorNames.CoinbaseWallet]: walletlink,
     [ConnectorNames.Ledger]: ledger,
-    [ConnectorNames.Portis]: portis,
-    [ConnectorNames.Fortmatic]: fortmatic
+    // [ConnectorNames.Portis]: portis,
+    // [ConnectorNames.Fortmatic]: fortmatic
 };
 
 export function getErrorMessage(error: Error) {
