0ecc8d0166c3c88c0086eb6a4d0639afd141b040
John Guthrie <jguthrie7@gmail.com>
2021-09-20 23:05:38 -0400

    WIP subgraph

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,28 +1,72 @@
+/// <reference path="../node_modules/assemblyscript/index.d.ts" />
+
 import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
 import { Deposit, Withdraw, OrderAnnounce, OrderMatched } from '../generated/ShrubExchange/ShrubExchange'
 import { Gravatar, Order, User } from '../generated/schema'
-import {BigInt, log} from '@graphprotocol/graph-ts'
-
+import {Address, BigDecimal, BigInt, log} from '@graphprotocol/graph-ts'
+import {getUser} from "./entities/user";
+import {getToken} from "./entities/token";
+import {getTokenBalance} from "./entities/tokenBalance";
+import {weiToEth} from "./utils"
+import {getOption} from "./entities/option";
 
 export function handleDeposit(event: Deposit): void {
-  let eventUser = event.params.user;
-  let amount = event.params.amount;
-  log.info('user: {} - amount: {}', [eventUser.toHex(), event.params.amount.toString()]);
-  let user = User.load(eventUser.toHex());
-  if (user === null) {
-    user = new User(eventUser.toHex());
-    user.balance = BigInt.fromI32(0);
-  }
-  user.balance = user.balance.plus(amount);
-  user.save();
+  let userAddress = event.params.user;
+  let tokenAddress = event.params.token;
+  log.info('user: {} - amount: {} - token: {}', [userAddress.toHex(), event.params.amount.toString(), tokenAddress.toHex()]);
+  let user = getUser(userAddress);
+  let token = getToken(tokenAddress);
+  // Convert amount to a decimal
+  // TODO: need to make this work with any number of decimals
+  // let amount = event.params.amount.divDecimal(BigDecimal.fromString(`1e18`));
+  let amount = weiToEth(event.params.amount);
+  let tokenBalance = getTokenBalance(userAddress, tokenAddress, event.block);
+  tokenBalance.block = event.block.number.toI32();
+  tokenBalance.timestamp = event.block.timestamp.toI32();
+  tokenBalance.unlockedBalance = tokenBalance.unlockedBalance.plus(amount);
+  tokenBalance.save();
 }
 
 export function handleWithdraw(event: Withdraw): void {
+  let userAddress = event.params.user;
+  let tokenAddress = event.params.token;
+  log.info('user: {} - amount: {} - token: {}', [userAddress.toHex(), event.params.amount.toString(), tokenAddress.toHex()]);
+  let user = getUser(userAddress);
+  let token = getToken(tokenAddress);
+  // Convert amount to a decimal
+  // TODO: need to make this work with any number of decimals
+  // let amount = event.params.amount.divDecimal(BigDecimal.fromString(`1e18`));
+  let amount = weiToEth(event.params.amount);
+  let tokenBalance = getTokenBalance(userAddress, tokenAddress, event.block);
+  tokenBalance.block = event.block.number.toI32();
+  tokenBalance.timestamp = event.block.timestamp.toI32();
+  tokenBalance.unlockedBalance = tokenBalance.unlockedBalance.minus(amount);
+  tokenBalance.save();
 
 }
 
 export function handleOrderAnnounce(event: OrderAnnounce): void {
+  let userAddress = event.params.user;
+  let smallOrder = event.params.order;
+  let common = event.params.common;
+  let positionHash = event.params.positionHash;
+  let txid = event.transaction.hash.toHex();
+  let logIndex = event.logIndex.toI32();
+  let id = txid + "-" + logIndex.toString();
 
+  let order = Order.load(id);
+  if (order === null) {
+    // We would expect this
+    order = new Order(id);
+    order.option = getOption(positionHash, common).id;
+    order.size = weiToEth(smallOrder.size);
+    order.isBuy = smallOrder.isBuy;
+    order.nonce = smallOrder.nonce.toI32();
+    order.price = weiToEth(smallOrder.price);
+    order.offerExpire = smallOrder.offerExpire.toI32();
+    order.fee = weiToEth(smallOrder.fee);
+    order.save();
+  }
 }
 
 export function handleOrderMatched(event: OrderMatched): void {
