1b17538345e149e16908deeceadc3f58548a3041
John Guthrie <jguthrie7@gmail.com>
2021-10-21 09:18:02 -0400

    WIP

--- packages/app/src/components/Positions.tsx
+++ packages/app/src/components/Positions.tsx
@@ -201,7 +201,7 @@ function Positions() {
     }
     console.log(shrubfolioData);
     setOptionsRows(tableRowsOptions);
-  }, [shrubfolioLoading])
+  }, [shrubfolioLoading, pendingTxsState])
 
   // options display
   // useEffect(() => {
