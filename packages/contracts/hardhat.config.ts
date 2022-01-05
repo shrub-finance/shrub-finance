951d07b91b7a9e992e9355dbd5bf0ac9ba30d4ce
way2rach <way2rachita@gmail.com>
2022-01-05 00:13:56 -0500

    upgrade to solidity 0.8.9

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -700,7 +700,7 @@ const config: HardhatUserConfig & AbiExporter = {
     deployer: 0,
   },
   solidity: {
-    version: "0.8.4",
+    version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,
