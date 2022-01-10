dc976b1b158e742985f3feed405d193b030cf299
way2rach <way2rachita@gmail.com>
2022-01-09 22:26:16 -0500

    update routes

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -25,10 +25,9 @@ function App() {
             <TopNav />
             <Router>
               <PaperView path="/" />
-              <HomeView path="/home" />
-              {/*<PaperView path="paper" />*/}
-              <PositionsView path="shrubfolio" />
-              <OptionsView path="options" />
+              {/*<HomeView path="/" />*/}
+              {/*<PositionsView path="shrubfolio" />*/}
+              {/*<OptionsView path="options" />*/}
             </Router>
           </Store>
         </ApolloProvider>
