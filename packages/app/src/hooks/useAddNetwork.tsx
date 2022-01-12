68677044fadeb310a347ac01133e8e0b4dc22628
way2rach <way2rachita@gmail.com>
2022-01-11 20:12:47 -0500

    modular useNetwork hook

--- packages/app/src/hooks/useAddNetwork.tsx
+++ packages/app/src/hooks/useAddNetwork.tsx
@@ -1,28 +1,13 @@
 import { handleErrorMessagesFactory } from "../utils/handleErrorMessages";
 import { useState } from "react";
+import { NETWORK_RPC_DETAILS } from "../constants/networks";
 
 export default function useAddNetwork() {
   const [localError, setLocalError] = useState("");
   const handleErrorMessages = handleErrorMessagesFactory(setLocalError);
 
   const addNetwork = () => {
-    const params = [
-      {
-        // chainId: "0x13881",
-        chainId: "0x89",
-        // chainName: "Matic Testnet",
-        chainName: "Matic Mainnet",
-        nativeCurrency: {
-          name: "Polygon Matic",
-          symbol: "Matic",
-          decimals: 18,
-        },
-        // rpcUrls: ["https://rpc-mumbai.maticvigil.com"],
-        rpcUrls: ["https://polygon-rpc.com/"],
-        // blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
-        blockExplorerUrls: ["https://polygonscan.com/"],
-      },
-    ];
+    const params = [NETWORK_RPC_DETAILS[80001]];
     // @ts-ignore
     window.ethereum
       // @ts-ignore
