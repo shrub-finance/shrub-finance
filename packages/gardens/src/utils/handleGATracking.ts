93ae54825b0b4adfc25a6ff6d27e8f6946767609
Queen Bud <way2rachita@gmail.com>
2022-04-01 18:53:02 -0400

    check if label is present (#339)

--- packages/gardens/src/utils/handleGATracking.ts
+++ packages/gardens/src/utils/handleGATracking.ts
@@ -1,19 +1,17 @@
-import ReactGA from 'react-ga'
-export function trackEvent(eventOption: { action: string, label: string }) {
-    const { action, label } = eventOption;
-    if (label.trim().length !== 0) {
-        ReactGA.event({
-            category: 'Clicked',
-            action: action,
-            label: label
-        });
-    }
-    else {
-        ReactGA.event({
-            category: 'Logo Clicked',
-            action: action,
-            label: label
-        });
-    }
-
+import ReactGA from "react-ga";
+export function trackEvent(eventOption: { action: string; label: string }) {
+  const { action, label } = eventOption;
+  if (label && label.trim().length !== 0) {
+    ReactGA.event({
+      category: "Clicked",
+      action: action,
+      label: label,
+    });
+  } else {
+    ReactGA.event({
+      category: "Logo Clicked",
+      action: action,
+      label: label,
+    });
+  }
 }
