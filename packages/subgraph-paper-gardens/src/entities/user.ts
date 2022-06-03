6dd09af2136bfa119621dd1f564e954ab7c76a22
John Guthrie <jguthrie7@gmail.com>
2022-06-02 23:16:54 -0400

    Add paperPot entities to subgraph

--- packages/subgraph-paper-gardens/src/entities/user.ts
+++ packages/subgraph-paper-gardens/src/entities/user.ts
@@ -1,5 +1,5 @@
 import { User } from '../../generated/schema'
-import { Address, BigInt } from '@graphprotocol/graph-ts'
+import { Address, BigInt, log } from '@graphprotocol/graph-ts'
 
 let Zero = BigInt.fromI32(0);
 let One = BigInt.fromI32(1);
@@ -8,6 +8,9 @@ export function createUser(address: Address): User {
   let user = new User(address.toHex());
   user.seedCount = Zero;
   user.ticketCount = Zero;
+  user.potCount = Zero;
+  user.waterCount = Zero;
+  user.fertilizerCount = Zero
   user.save();
   return user;
 }
@@ -42,6 +45,49 @@ export function incrementTicketCount(user: User, amount: BigInt): User {
 
 export function decrementTicketCount(user: User, amount: BigInt): User {
   user.ticketCount = user.ticketCount.minus(amount);
+  user.save();
+  return user;
+}
+
+// Pots
+export function incrementPotCount(user: User, amount: BigInt): User {
+  log.info("running incrementPotCount",[]);
+  log.info("amount: {}", [amount.toString()])
+  log.info("user.id: {}", [user.id])
+  log.info("potCount: {}, amount: {}", [user.potCount.toString(), amount.toString()])
+  user.potCount = user.potCount.plus(amount);
+  user.save();
+  return user;
+}
+
+export function decrementPotCount(user: User, amount: BigInt): User {
+  user.potCount = user.potCount.minus(amount);
+  user.save();
+  return user;
+}
+
+// Water
+export function incrementWaterCount(user: User, amount: BigInt): User {
+  user.waterCount = user.waterCount.plus(amount);
+  user.save();
+  return user;
+}
+
+export function decrementWaterCount(user: User, amount: BigInt): User {
+  user.waterCount = user.waterCount.minus(amount);
+  user.save();
+  return user;
+}
+
+// Fertilizer
+export function incrementFertilizerCount(user: User, amount: BigInt): User {
+  user.fertilizerCount = user.fertilizerCount.plus(amount);
+  user.save();
+  return user;
+}
+
+export function decrementFertilizerCount(user: User, amount: BigInt): User {
+  user.fertilizerCount = user.fertilizerCount.minus(amount);
   user.save();
   return user;
 }
---
+++ packages/subgraph-paper-gardens/src/mapping-paper-pot.ts
@@ -0,0 +1,125 @@
+import { BigInt, log } from '@graphprotocol/graph-ts'
+import { ZERO_ADDRESS } from '@protofire/subgraph-toolkit'
+
+import {
+  decrementFertilizerCount,
+  decrementPotCount,
+  decrementTicketCount, decrementWaterCount,
+  getUser, incrementFertilizerCount,
+  incrementPotCount,
+  incrementTicketCount, incrementWaterCount,
+} from './entities/user'
+import { Plant, Grow, TransferBatch, TransferSingle } from '../generated/PaperPot/PaperPot'
+import { User } from '../generated/schema'
+let One = BigInt.fromI32(1);
+let Two = BigInt.fromI32(2);
+let Three = BigInt.fromI32(3);
+let OneMillion = BigInt.fromI32(1000000);
+let TwoMillion = BigInt.fromI32(2000000);
+let ThreeMillion = BigInt.fromI32(3000000);
+
+// - ApprovalForAll(indexed address,indexed address,bool)
+// - Grow(uint256,uint16,uint16)
+// - Plant(uint256,uint256,address)
+// - TransferBatch(indexed address,indexed address,indexed address,uint256[],uint256[])
+// - TransferSingle(indexed address,indexed address,indexed address,uint256,uint256)
+// - URI(string,indexed uint256)
+
+export function handleTransferSingle(event: TransferSingle): void {
+  let operator = event.params.operator
+  let tokenId = event.params.id
+  let from = event.params.from
+  let to = event.params.to
+  let value = event.params.value
+  let fromUser: User
+  let toUser: User
+  // 3 Cases
+  // Mint, Burn, Transfer
+  if (from.toHexString() != ZERO_ADDRESS) {
+    fromUser = getUser(from)
+  }
+  if (to.toHexString() != ZERO_ADDRESS) {
+    toUser = getUser(to)
+  }
+
+  log.info('tokenId: {}, from: {}, to: {}, value: {}', [tokenId.toString(), from.toHexString(), to.toHexString(), value.toString()]);
+
+  if (tokenId == One) {
+    log.info('its a pot', []);
+    // Case: Pot
+    if (from.toHexString() != ZERO_ADDRESS) {
+      log.info('decrementing', []);
+      decrementPotCount(fromUser, value);
+    }
+    if (to.toHexString() != ZERO_ADDRESS) {
+      log.info('incrementing', []);
+      incrementPotCount(toUser, value);
+    }
+  } else if (tokenId == Two) {
+    // Case: Fertilizer
+    if (from.toHexString() != ZERO_ADDRESS) {
+      decrementFertilizerCount(fromUser, value);
+    }
+    if (to.toHexString() != ZERO_ADDRESS) {
+      incrementFertilizerCount(toUser, value);
+    }
+  } else if (tokenId == Three) {
+    // Case: Water
+    if (from.toHexString() != ZERO_ADDRESS) {
+      decrementWaterCount(fromUser, value);
+    }
+    if (to.toHexString() != ZERO_ADDRESS) {
+      incrementWaterCount(toUser, value);
+    }
+  } else if (tokenId >= OneMillion) {
+    // Case: Potted Plant
+  } else if (tokenId >= TwoMillion && tokenId < ThreeMillion) {
+    // Case: Shrub
+  } else {
+    // This shouldn't happen
+    log.error('unexpected tokenId: {}', [tokenId.toString()]);
+  }
+// if (tokenId != One) {
+  //   log.info('unexpected tokenId: {}', [tokenId.toString()]);
+  //   return;
+  // }
+  // if (from.toHexString() != ZERO_ADDRESS) {
+  //   let fromUser = getUser(from)
+  //   decrementTicketCount(fromUser, value)
+  // }
+  // if (to.toHexString() != ZERO_ADDRESS) {
+  //   let toUser = getUser(to)
+  //   incrementTicketCount(toUser, value)
+  // }
+}
+export function handleTransferBatch(event: TransferBatch): void {
+  let operator = event.params.operator
+  let tokenIds = event.params.ids
+  let from = event.params.from
+  let to = event.params.to
+  let values = event.params.values
+
+  for (let i = 0; i < values.length; i++) {
+    // let tokenId = tokenIds[i];
+    // let value = values[i];
+    // if (tokenId == One) {
+    //   // Only count for the first tokenId
+    //   if (from.toHexString() != ZERO_ADDRESS) {
+    //     let fromUser = getUser(from)
+    //     decrementTicketCount(fromUser, value)
+    //   }
+    //   if (to.toHexString() != ZERO_ADDRESS) {
+    //     let toUser = getUser(to)
+    //     incrementTicketCount(toUser, value)
+    //   }
+    // }
+  }
+}
+
+export function handleGrow(event: Grow): void {
+
+}
+
+export function handlePlant(event: Plant): void {
+
+}
