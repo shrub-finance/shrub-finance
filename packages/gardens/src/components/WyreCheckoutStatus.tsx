1df97f5d15c1dd47e48ccb1f6182fd1809c2eac6
way2rach <way2rachita@gmail.com>
2022-06-04 00:48:44 -0400

    update to brand colors

--- packages/gardens/src/components/WyreCheckoutStatus.tsx
+++ packages/gardens/src/components/WyreCheckoutStatus.tsx
@@ -1,53 +1,62 @@
-import { 
-    Alert, 
-    AlertIcon, 
-    AlertTitle, 
-    AlertDescription, 
-    Box, 
-    CloseButton, 
-    Center, 
-    useDisclosure 
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Box,
+  CloseButton,
+  Center,
+  Text,
+  useDisclosure,
+  SlideFade,
 } from "@chakra-ui/react";
+import React from "react";
 
 function WyreCheckoutStatus() {
-    // look for a wyreCheckoutStatus query param in the url
-    const currentURL = new URL(window.location.href);
-    const currentURLParams = new URLSearchParams(currentURL.search);
-    const wyreCheckoutStatus = currentURLParams.get('wyreCheckoutStatus');
-    // create alert params based on wyreCheckoutStatus
-    let description;
-    let status: "error" | undefined;
-    switch (wyreCheckoutStatus) {
-        case 'failure':
-            status = 'error';
-            description = <>
-                There was an error while attempting to complete your transaction.<br />
-                Contact Wyre support for more information at support@sendwyre.com
-            </>
-            break;
-        default:
-            break;
-    }
-    // create an alert that will display a message on redirects according to the checkout status
-    const defaultIsOpen = !!status;
-    const { isOpen, onClose } = useDisclosure({ defaultIsOpen });
-    return isOpen ? (
-        <Center>
-            <Box position={"fixed"} w={"auto"} top={20}>
-                <Alert status={status} variant={"solid"}>
-                    <AlertIcon />
-                    <Box>
-                        <AlertTitle>Wyre Checkout</AlertTitle>
-                        <AlertDescription>{description}</AlertDescription>
-                    </Box>
-                    <CloseButton
-                        alignSelf='flex-start'
-                        onClick={onClose}
-                    />
-                </Alert>
-            </Box>
-        </Center>
-    ) : null;
+  // look for a wyreCheckoutStatus query param in the url
+  const currentURL = new URL(window.location.href);
+  const currentURLParams = new URLSearchParams(currentURL.search);
+  const wyreCheckoutStatus = currentURLParams.get("wyreCheckoutStatus");
+
+  // create alert params based on wyreCheckoutStatus
+  let description;
+  let status: "error" | undefined;
+  switch (wyreCheckoutStatus) {
+    case "failure":
+      status = "error";
+      description = (
+        <>
+          <Text>
+            There was an error while attempting to complete your transaction.
+          </Text>
+          <Text>
+            Contact Wyre support for more information at support@sendwyre.com
+          </Text>
+        </>
+      );
+      break;
+    default:
+      break;
+  }
+
+  // create an alert that will display a message on redirects according to the checkout status
+  const defaultIsOpen = !!status;
+  const { isOpen, onClose } = useDisclosure({ defaultIsOpen });
+
+  return isOpen ? (
+    <Center mt={20}>
+      <SlideFade in={true} unmountOnExit={true}>
+        <Alert status={status} borderRadius={9}>
+          <AlertIcon />
+          <Box>
+            <AlertTitle>Wyre Checkout</AlertTitle>
+            <AlertDescription>{description}</AlertDescription>
+          </Box>
+          <CloseButton alignSelf="flex-start" onClick={onClose} />
+        </Alert>
+      </SlideFade>
+    </Center>
+  ) : null;
 }
 
 export default WyreCheckoutStatus;
