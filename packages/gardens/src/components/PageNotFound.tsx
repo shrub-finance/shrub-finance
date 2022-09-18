575a2c0dd0143be84d9d94d3b9ab4c91e3bd0996
neeta20 <neetta.dhanak@gmail.com>
2022-09-18 15:52:28 -0400

    enhance 404 page
    enhance 404 page

--- packages/gardens/src/components/PageNotFound.tsx
+++ packages/gardens/src/components/PageNotFound.tsx
@@ -3,11 +3,24 @@ import {
   Box,
   Center,
   Container,
+  Flex,
   Heading,
+  Link,
+  Spacer,
+  Image,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Tr,
 } from "@chakra-ui/react";
 import React from "react";
 import { trackEvent } from "../utils/handleGATracking";
 import { Link as ReachLink } from "@reach/router";
+import { IMAGE_ASSETS } from "../utils/imageAssets";
+import StatsBox from "./StatsBox";
 
 function PageNotFound() {
   function handleGA(event: any) {
@@ -17,25 +30,57 @@ function PageNotFound() {
     });
   }
   return (
-    <Container
+    
+   <Container
       mt={isMobile ? 30 : 50}
       p={5}
       flex="1"
       borderRadius="2xl"
       maxW="container.lg"
     >
-      <Center h='600'>
-        <Box maxW="60rem" mb={8} textAlign={"center"}>
-          <Heading
-            fontSize={{ base: "30px", md: "50px" }}
-            letterSpacing={"tight"}
-          >
-            Error 404  : Page Not Found        
-          </Heading>
-        </Box>
-      </Center>
-    </Container>
-  );
-}
 
+      <Box maxW="60rem"  textAlign={"center"}>
+        <Text pt="8">
+          Oops ....  Error 404  : Page Not Found
+        </Text>
+      </Box> 
+      
+      <Flex
+          direction={{ base: "column", md: "column", lg: "column", xl: "row" }}
+        >
+          <Box mt={{ base: 10, md: 16, lg: 16, xl: 24 }} mb={{base: 0, md:0, lg: 0, xl:0 }} >
+            <Heading 
+              fontSize={{ base: "30px", md: "81px" }}
+              letterSpacing={"tight"}
+         //   maxW={{ base: "20rem", md: "40rem" }}
+              textAlign={{ base: "center", md: "center", lg:"left", xl: "left"}}
+            >
+              Lost in the <br />
+              <Text
+                as="span"
+                background="gold.100"
+                bgClip="text" 
+              >
+                Darkness
+              </Text>{" "}
+            </Heading>
+          </Box>
+          <Spacer />
+          <Box
+            mt={{ base: 10, md: 16, lg: 16, xl: 16}}
+       //     ml={{ md: 30, lg: 80, xl: 0 }}
+            display={{ base: "block", md: "block" }}
+          >
+              <Image
+          //      width={{ base: "32rem", md: "42rem" }}
+                src="lost-seeds.svg"
+                fallbackSrc="lost-seeds.svg"
+                alt=""
+              />
+          </Box>
+</Flex>
+
+</Container>
+    )
+    }
 export default PageNotFound;
