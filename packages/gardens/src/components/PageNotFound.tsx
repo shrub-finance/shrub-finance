fc44b203c9a0cf270fd2ac70f7d9cdadcf422f3c
Queen Bud <way2rachita@gmail.com>
2022-09-20 15:35:34 -0400

    resolve conflicts (#472)

--- packages/gardens/src/components/PageNotFound.tsx
+++ packages/gardens/src/components/PageNotFound.tsx
@@ -1,13 +1,20 @@
 import { isMobile } from "react-device-detect";
 import {
   Box,
-  Center,
   Container,
+  Flex,
   Heading,
+  Spacer,
+  Image,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Tr,
 } from "@chakra-ui/react";
 import React from "react";
 import { trackEvent } from "../utils/handleGATracking";
-import { Link as ReachLink } from "@reach/router";
 
 function PageNotFound() {
   function handleGA(event: any) {
@@ -24,18 +31,106 @@ function PageNotFound() {
       borderRadius="2xl"
       maxW="container.lg"
     >
-      <Center h='600'>
-        <Box maxW="60rem" mb={8} textAlign={"center"}>
+      <Box maxW="60rem" textAlign={"center"}>
+        <Text pt="8">Oops .... Page Not Found</Text>
+      </Box>
+
+      <Flex
+        direction={{ base: "column", md: "column", lg: "column", xl: "row" }}
+      >
+        <Box
+          mt={{ base: 10, md: 16, lg: 16, xl: 24 }}
+          mb={{ base: 0, md: 0, lg: 0, xl: 0 }}
+        >
           <Heading
-            fontSize={{ base: "30px", md: "50px" }}
+            fontSize={{ base: "30px", md: "81px" }}
             letterSpacing={"tight"}
+            mr={30}
+            textAlign={{ base: "center", md: "center", lg: "left", xl: "left" }}
           >
-            Error 404  : Page Not Found        
+            Lost in the <br />
+            <Text as="span" background="gold.100" bgClip="text">
+              Darkness
+            </Text>{" "}
           </Heading>
         </Box>
-      </Center>
+        <Spacer />
+        <Box
+          mt={{ base: 10, md: 16, lg: 16, xl: 16 }}
+          display={{ base: "block", md: "block" }}
+        >
+          <Image
+            src="https://shrub.finance/assets/lost-seeds.svg"
+            fallbackSrc="https://shrub.finance/assets/lost-seeds.svg"
+            alt="404 page image"
+            rounded={"lg"}
+          />
+        </Box>
+      </Flex>
+
+      <Text
+        fontSize={{ base: "10px", md: "24px" }}
+        letterSpacing={"tight"}
+        textAlign={"center"}
+        mt={{ base: 10, md: 16, lg: 16, xl: 20 }}
+        mb={{ base: 10, md: 16, lg: 10, xl: 10 }}
+      >
+        Here are some of the pages that might interest you
+      </Text>
+
+      <TableContainer>
+        <Table>
+          <Tbody>
+            <Tr>
+              <Td display={{ base: "table-cell", md: "table-cell" }}>
+                <Text as="span" background="gold.100" bgClip="text">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://exchange.shrub.finance/"
+                  >
+                    Shrub Exchange
+                  </a>
+                </Text>
+              </Td>
+              <Td display={{ base: "table-cell", md: "table-cell" }}>
+                <Text as="span" background="gold.100" bgClip="text">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://medium.com/@shrubfinance/shrub-roadmap-2022-b947b5ce1435"
+                  >
+                    Roadmap 2022
+                  </a>
+                </Text>
+              </Td>
+              <Td display={{ base: "table-cell", md: "table-cell" }}>
+                <Text as="span" background="gold.100" bgClip="text">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://docs.shrub.finance/"
+                  >
+                    Docs
+                  </a>
+                </Text>
+              </Td>
+              <Td display={{ base: "table-cell", md: "table-cell" }}>
+                <Text as="span" background="gold.100" bgClip="text">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://shrub.finance/"
+                  >
+                    Shrub Main
+                  </a>
+                </Text>
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 }
-
 export default PageNotFound;
